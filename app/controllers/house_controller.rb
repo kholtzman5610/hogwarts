cf4c8d1da01386dfdd18ffefd36eee1fa86e5c61
kholtzman5610 <kassidy.holtzman@outlook.com>
2021-05-17 14:13:54 -0400

    update

---
+++ app/controllers/house_controller.rb
@@ -0,0 +1,32 @@
+class HouseController < ApplicationController
+    before_action :set_house, only: [:show, :edit, :update, :destroy]
+    def index
+        @houses = House.all
+    end
+
+    def show
+    end
+
+    def new
+        @house = House.new
+    end
+
+    def create
+        @house = House.new(house_params)
+        if @house.save
+            redirect_to @house
+        else
+            render :new
+        end
+    end
+
+
+    private
+    def set_house
+        @house = House.find(params[:id])
+    end
+
+    def house_params
+        params.require(:house).permirt(:name)
+    end
+end
---
+++ app/controllers/professor_controller.rb
@@ -0,0 +1,32 @@
+class ProfessorController < ApplicationController
+    before_action :set_professor, only: [:show, :edit, :update, :destroy] 
+    def index
+      @professors = Professor.all
+    end
+      
+    def show
+    end
+  
+    def new
+      @professor = Professor.new
+    end
+  
+    def create
+      @professor = Professor.new(professor_params)
+      if @professor.save
+        redirect_to @professor
+      else
+        render :new
+      end
+    end
+      
+      
+    private
+    def set_professor
+      @professor = Professor.find(params[:id])
+    end
+  
+    def professor_params
+      params.require(:professor).permirt(:first_name, :last_name, :house_id)
+    end
+end

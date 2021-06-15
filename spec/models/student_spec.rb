670564d7a48fb8816cedb8a6c5b9d07ed0b4ce01
kholtzman5610 <kassidy.holtzman@outlook.com>
2021-06-15 13:21:42 -0400

    validations

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  subject { Student.new(first_name: "Jack", last_name: "Smith", house_id: house.id)}  
+  subject { Student.new(first_name: "Jack", last_name: "Smith", house_id: 1)}  
 
   it "is valid with valid attributes" do
+    House.create(name:'Gryffindor')
     expect(subject).to be_valid
   end
 
@@ -14,6 +15,11 @@ RSpec.describe Student, type: :model do
   
   it "is not valid without a last_name" do
     subject.last_name=nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a house_id" do
+    subject.house_id=nil
     expect(subject).to_not be_valid
   end
 end

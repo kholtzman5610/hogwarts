9226997e43f23b2edc4ec46c142975e4aa110842
kholtzman5610 <kassidy.holtzman@outlook.com>
2021-06-25 18:10:00 -0400

    reverting back

--- app/models/grade.rb
+++
@@ -1,4 +0,0 @@
-class Grade < ApplicationRecord
-    belongs_to :cohort
-    belongs_to :student
-end
--- spec/factories/grades.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :grade do
-    student_id { 1 }
-    cohort_id { 1 }
-    mark { "MyString" }
-  end
-end

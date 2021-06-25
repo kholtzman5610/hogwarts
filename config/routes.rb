20135325d4d1abbd1e4245f8c637e856f0ffde20
kholtzman5610 <kassidy.holtzman@outlook.com>
2021-06-24 23:15:57 -0400

    finished setting up devise + styling updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
-
+  root 'home#index'
+  
   resources :cohorts
   resources :courses
   resources :house
   resources :professors
   resources :students
-
-  root 'home#index'
 end

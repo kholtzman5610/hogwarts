65d2b6366322a60d1bcdc5109bc274ffc199b0a3
kholtzman5610 <kassidy.holtzman@outlook.com>
2021-06-30 13:37:45 -0400

    update models

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,7 +1,7 @@
 class Professor < ApplicationRecord
+    belongs_to :cohort
     belongs_to :course
     belongs_to :house
-    belongs_to :cohort
 
     def full_name
         "#{first_name} #{last_name}"
